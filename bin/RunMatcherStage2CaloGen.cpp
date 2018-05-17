af8802d243cb2a9deb2119cdcc6ee5cf65129952
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-05-17 11:25:40 +0100

    Included the header required for calo jet quantities as a complete example and referenced it in the cpp files

--- bin/RunMatcherStage2CaloGen.cpp
+++ bin/RunMatcherStage2CaloGen.cpp
@@ -52,7 +52,7 @@ namespace fs = boost::filesystem;
  * 
  * Warning: the header file that recognises the leaves for the reco/calo objects (L1TNtuples/interface/L1AnalysisRecoJetDataFormat.h) may not include native support for the calo jet leaves you need.
    Check the header file for references to "caloEt", "caloEta" and "caloPhi".
-   If they're not included, add the lines `calo${quantity}.clear();` and `std::vector<float> calo${quantity};` in the relevant places. Then, recompile.
+   If they're not included, add the lines `calo${quantity}.clear();` and `std::vector<float> calo${quantity};` in the relevant places (see Header_for_CaloJetQuantities/). Then, recompile.
  * You may also have to comment out the if() conditions in the function "findRecoJetIndex" in src/runMatcherUtils.cc
  */
 int main(int argc, char* argv[]) {
